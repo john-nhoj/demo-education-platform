4fce3d275180359eef3080f4367ebf9dd85ff6fd
Private <cheng.john@pm.me>
2021-06-10 21:38:06 +0300

    Add a custom sign in page

---
+++ pages/auth/signin.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ClientSafeProvider, getProviders, signIn } from 'next-auth/client';
+
+interface SignInProps {
+  providers: Record<string, ClientSafeProvider>;
+}
+
+const SignIn = ({ providers }: SignInProps) => {
+  console.debug(providers);
+  return (
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
+
+export default SignIn;
