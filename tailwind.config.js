8207ffff05312a6f886fc6ca0896e1cbbcf79ed8
Private <cheng.john@pm.me>
2021-06-11 10:44:36 +0300

    Add tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{ts,tsx}', './styles/**/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
