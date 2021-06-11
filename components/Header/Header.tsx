243518c2aaf4a17d908b7c6f73bd56bb19afbdfe
Private <cheng.john@pm.me>
2021-06-11 15:54:14 +0300

    Add signout in header and logged in user info

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
 import * as cn from 'classnames';
+import { signOut, useSession } from 'next-auth/client';
 
 const Header = () => {
+  const [session] = useSession();
+
   return (
-    <header className={cn('header', 'bg-red-400')}>
-      This is the header
+    <header
+      className={cn('header', 'grid', 'items-center', 'px-8', 'bg-red-400')}
+    >
+      {session ? (
+        <HeaderContent user={session.user} />
+      ) : (
+        'You are not logged in.'
+      )}
       <style>{`
         .header {
           grid-area: header;
         }
       `}</style>
     </header>
+  );
+};
+
+const HeaderContent = ({
+  user,
+}: {
+  user: {
+    name?: string;
+    email?: string;
+    image?: string;
+  };
+}) => {
+  return (
+    <div className={cn('grid', 'header-content')}>
+      <div className={cn('info', 'flex', 'items-center')}>
+        You are logged in as {user.name || user.email}
+        {user.image && (
+          <img
+            src={user.image}
+            alt=""
+            className={cn('h-10', 'w-10', 'rounded-full', 'ml-4')}
+          />
+        )}
+      </div>
+      <button onClick={() => signOut()}>Sign out</button>
+      <style>{`
+        .header-content {
+          grid-template-columns: auto;
+          grid-template-areas:
+            'info signout';
+        }
+
+        .info {
+          grid-area: info;
+        }
+
+        button {
+          grid-area: signout;
+        }
+      `}</style>
+    </div>
   );
 };
 
